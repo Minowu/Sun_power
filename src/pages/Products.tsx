fadcfd5e7529dac8230658fbb050db25b9138e3e
Tạ Minh Công <cong23637@gmail.com>
2025-10-21 20:38:48 +0700

    Tạm thời xong trang product

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -44,6 +44,10 @@ const Products = () => {
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">   
       <div className="px-4 sm:px-6 lg:px-8 py-8 grid grid-cols-4 gap-2 mt-2" style={{gridTemplateRows: 'auto 1000px'}}>
         {/* Primary Filters - Top Row (Red) */}
+        <div className="relative col-span-1 col-start-1 align-bottom">
+          <h3 className="absolute bottom-0 left-5 text-black text-2xl font-semibold mb-4 ">{currentCategory?.name}</h3>
+          <div className="absolute bottom-0 left-0 w-full h-1 bg-black"></div>
+        </div>
         <div className="mb-6 col-span-3 col-start-2">
           <div className="grid grid-cols-4 gap-4">
             {categories.map((category) => (
@@ -76,8 +80,6 @@ const Products = () => {
         <div className="row-start-2 grid ">
           {/* Secondary Filters - Left Column */}
             <div className="rounded-lg p-4">
-              <h3 className="text-black text-xl font-semibold mb-4 ">{currentCategory?.name}</h3>
-              <div className="w-full h-1 bg-black"></div>
               <div className="space-y-2">
                 {currentCategory?.subCategories
                   .filter(sub => sub !== 'all')
