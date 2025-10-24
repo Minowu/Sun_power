17e93b1a6b851b3f5835d1399422133dc0578e58
Minowu <cong23637@gmail.com>
2025-10-24 14:46:02 +0700

    xong solution detail

--- src/components/solution/FeatureBlock.tsx
+++ src/components/solution/FeatureBlock.tsx
@@ -17,9 +17,9 @@ const FeatureBlock = ({ title, detail1, detail2, image }: FeatureBlockProps) => 
       viewport={{ once: true }}
     >
       <div className="flex flex-col items-center gap-8">
-        <div className="w-full flex justify-between items-start gap-8 px-12 pt-30 max-w-7xl">
+        <div className="w-full flex justify-between items-start gap-8 px-12 pt-30 max-w-7xl max-md:text-xs max-md:px-0 max-md:pt-10">
           <div className="flex-1">
-            <h3 className="text-5xl text-gradient-teal-blue font-bold">
+            <h3 className="text-5xl text-gradient-teal-blue font-bold max-md:text-3xl">
               {title}
             </h3>
           </div>
@@ -36,7 +36,7 @@ const FeatureBlock = ({ title, detail1, detail2, image }: FeatureBlockProps) => 
           <img 
             src={image} 
             alt={title}
-            className="w-full h-[600px] rounded-lg"
+            className="w-full h-[600px] rounded-lg max-md:h-full"
           />
         </div>
       </div>
